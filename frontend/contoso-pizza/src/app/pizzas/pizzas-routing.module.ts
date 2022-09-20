fff8226a961189aebde09371dff4291bb929561d
arpafer <antonio.antoniopf@gmail.com>
2022-09-21 00:10:52 +0100

    Se añade modulo pizzas

--- frontend/contoso-pizza/src/app/pizzas/pizzas-routing.module.ts
+++ frontend/contoso-pizza/src/app/pizzas/pizzas-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PizzasModule } from './pizzas/pizzas.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), PizzasModule],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class PizzasRoutingModule { }
